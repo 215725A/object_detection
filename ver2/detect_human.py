82827b2b8e22ed13ab6d18f581388a7a027b8279
215725A <e215725@ie.u-ryukyu.ac.jp>
2024-06-24 02:14:34 +0900

    並列処理による人検知の実装

--- ver2/detect_human.py
+++ ver2/detect_human.py
@@ -1,33 +1,115 @@
+# Installed Libraries
+import cv2
+
 # Standard Libraries
 import argparse
 import time
+from multiprocessing import Process, Queue
 
 # Self-made Libraries
 import util.app as app
+from util.yaml import read
 
-def main(arg):
-    # Initialize
-    detector = app.VideoDetector()
 
-    # Load Settings
-    detector.roadConfigFile(arg)
+def process_detector(frame_queue, result_queue, model_path):
+    detector = app.VideoDetector(model_path)
+    while True:
+        frame_number, frame = frame_queue.get()
+        if frame is None:
+            break
 
-    # Set up
-    detector.setupVideoCapture()
-    detector.setupVideoWriter()
-    detector.setupModel()
+        result = detector.detectHuman(frame)
+        result_queue.put((frame_number, result))
 
-    # Try Detect
-    detector.detectHumanBody()
+def load_settings(config_path):
+    config = read(config_path)
+    process_num = config['process_num']
+    model_path = config['model']
+    video_path = config['video_path']
+    output_path = config['video_output_path']
+
+    return process_num, model_path, video_path, output_path
+
+def setUpCap(video_path):
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        raise ValueError("""
+                             VideoCapture not opened. \n
+                             Check if the 'movie_path' is correct and ffmpeg or gstreamer is properly installed.
+                            """
+        )
+
+    return cap
+
+def setUpQueue():
+    frame_queue = Queue()
+    result_queue = Queue()
+    return frame_queue, result_queue
+
+def setUpWriter(cap, output_path):
+    cap_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    cap_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    writer = cv2.VideoWriter(output_path, fourcc, fps, (cap_width, cap_height))
+    return writer
+
+def saveVideo(writer, results):
+    if results:
+        for frame in results:
+            writer.write(frame)
+        writer.release()
+
+def main(config_path):
+    start = time.perf_counter()
+
+    # Settings
+    process_num, model_path, video_path, output_path = load_settings(config_path)
+
+    # Setup
+    cap = setUpCap(video_path)
+    frame_queue, result_queue = setUpQueue()
+    writer = setUpWriter(cap, output_path)
+
+    # Setup Workers
+    detectors = [
+        Process(target=process_detector, args=(frame_queue, result_queue, model_path))
+        for _ in range(process_num)
+    ]
+
+    for detector in detectors:
+        detector.start()
+    
+    frame_number = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_queue.put((frame_number, frame))
+        frame_number += 1
+    
+    for _ in detectors:
+        frame_queue.put((None, None))
+    
+    results = [None] * frame_number
+    for _ in range(frame_number):
+        frame_number, result = result_queue.get()
+        results[frame_number] = result
+    
+    for detector in detectors:
+        detector.join()
+    
+    cap.release()
+
+    saveVideo(writer, results)
+
+    end = time.perf_counter()
+    print(f"実行時間: {end - start:.2f}s")
 
 if __name__ == '__main__':
-    start = time.perf_counter()
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', help='Load YML')
 
     args = parser.parse_args()
 
     main(args.config)
-    end = time.perf_counter()
-
-    print(f"実行時間: {end - start:.2f}s")
--- ver2/util/app.py
+++ ver2/util/app.py
@@ -1,89 +1,43 @@
-# Installed Libraly
+# Installed Libraries
 import cv2
 from ultralytics import YOLO
 
-# Self-made Libraly
-from . import yaml
-
 class VideoDetector:
-    def __init__(self):
-        self.cneter_points = []
-
-    def setupVideoCapture(self):
-        video_path = self.config['video_path']
-        self.cap = cv2.VideoCapture(video_path)
-        if not self.cap.isOpened():
-            raise ValueError("""
-                             VideoCapture not opened. \n
-                             Check if the 'movie_path' is correct and ffmpeg or gstreamer is properly installed.
-                            """
-            )
-        self.cap_witdh = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.cap_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
     
-    def setupVideoWriter(self):
-        output_path = self.config['video_output_path']
-        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        self.writer = cv2.VideoWriter(output_path, self.fourcc, self.fps, (self.cap_witdh, self.cap_height))
-    
-    def setupModel(self):
-        model = self.config['model']
-        self.model = YOLO(model)
-
-    def release(self):
-        self.writer.release()
-        self.cap.release()
-        cv2.destroyAllWindows()
-
-    def detectHumanBody(self):
+    def detectHuman(self, frame):
         try:
-            while self.cap.isOpened():
-                ret, self.frame = self.cap.read()
+            detected = self.model(frame)
+            boxes = detected[0].boxes
 
-                if not ret:
-                    break
+            for box in boxes:
+                cls = box.cls
+                conf = box.conf
+                xyxy = box.xyxy
 
-                detected = self.model(self.frame)
-                boxes = detected[0].boxes
+                if self.model.names[int(cls)] == 'person' and conf >= 0.5:
+                    frame = self.drawRectAngle(frame, conf, xyxy)
 
-                for box in boxes:
-                    cls = box.cls
-                    conf = box.conf
-                    xyxy = box.xyxy
-                
-                    if self.model.names[int(cls)] == 'person' and conf >= 0.5:
-                        self.drawRectAngle(conf, xyxy)
-
-                self.saveVideo(self.frame)
+            return frame
 
         except cv2.error as e:
             print(e)
-        
-        self.release()
-
-    def drawRectAngle(self, conf, xyxy):
+    
+    def drawRectAngle(self, frame, conf, xyxy):
         for _xyxy, _conf in zip(xyxy, conf):
             x1, y1, x2, y2 = map(int, _xyxy)
             label = f'person: {_conf:.2f}'
-            cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-            cv2.putText(self.frame, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            center_x = (x1 + x2) // 2
-            center_y = (y1 + y2) // 2
-            # self.drawCenterPoint(center_x, center_y)
-            self.drawObit((center_x, center_y))
 
-    # def drawCenterPoint(self, center_x, center_y):
-    #     cv2.circle(self.frame, (center_x, center_y), 2, (0, 255, 0), 3)
-    
-    def drawObit(self, new_center_point):
-        self.cneter_points.append(new_center_point)
-        for points in self.cneter_points:
-            cv2.circle(self.frame, points, 2, (0, 255, 0), 3)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.putText(frame, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    def saveVideo(self, frame):
-        self.writer.write(frame)
+            center = ((x1+x2) // 2, (y1+y2) // 2)
+            frame = self.drawCenterPoint(frame, center)
 
-    def roadConfigFile(self, file_path):
-        config_path = f'{file_path}'
-        self.config = yaml.read(config_path)
+        return frame
+
+    def drawCenterPoint(self, frame, center):
+        frame = cv2.circle(frame, center, 2, (0, 255, 0), 3)
+        
+        return frame
