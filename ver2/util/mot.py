3648b9f1b64faa9e14f19e4cd82e334804307a76
NakadaTomoya <t.nakada@churadata.okinawa>
2024-07-26 19:14:13 +0900

    タイポの修正と不必要な機能のコメントアウト

--- ver2/util/mot.py
+++ ver2/util/mot.py
@@ -6,16 +6,16 @@ import random
 
 class MOT:
     def __init__(self, dt=0.1):
-        self.tracker = MultiObjectTracker(dt=dt, tracker_kwargs={'max_staleness': 10})
-        self.coloers = {}
+        self.tracker = MultiObjectTracker(dt=dt, tracker_kwargs={'max_staleness': 60})
+        self.colors = {}
     
     def update(self, detections):
         self.tracker.step(detections=detections)
         return self.tracker.active_tracks()
     
     def get_color(self, track_id):
-        if track_id not in self.coloers:
-            # Generate Random Coloer
-            self.coloers[track_id] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        if track_id not in self.colors:
+            # Generate Random Color
+            self.colors[track_id] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         
-        return self.coloers[track_id]
+        return self.colors[track_id]
